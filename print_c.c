59cccd02929f90ca3b24e85cbe420ca11c5616a4
Nomen_collab <nomenaratsimba01@gmail.com>
2025-03-26 12:58:10 +0000

    update script print_c.c

--- print_c.c
+++ print_c.c
@@ -2,17 +2,16 @@
 
 /**
  * print_char - prints a char.
- *@val: Argument
- *Return: 1
-*/
+ * @val: Argument
+ * Return: 1
+ */
 
 int print_char(va_list val)
 {
 	char str;
-	
+
 	str = va_arg(val, int);
-	
 	_putchar(str);
-	
+
 	return (1);
-}	
+}
